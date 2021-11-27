2e0340e592085bcae65fbb3a6f463bccec22f989
Jules <julesdorbeau@hotmail.fr>
2021-11-27 13:45:54 +0100

    pc portable push test

--- data_treatment/dataframe_functions_use.py
+++ data_treatment/dataframe_functions_use.py
@@ -7,19 +7,25 @@ Created on Sat Nov 27 09:33:11 2021
 
 import pandas as pd
 
-df = pd.read_csv("generated_dataframe.csv")
-pd.set_option('display.max_columns', None)
-
-
 def region_selection(df, region_choice):
     return df.loc[df['region'] == region_choice]
 
+df = pd.read_csv("generated_dataframe.csv")
+pd.set_option('display.max_columns', None)
+print("========== DATAFRAME SANS MODIFICATIONS ==========")
+print(df.head())
+print()
 
-print("===== BRETAGNE REGION SELECTION ======")
+
+print("========== BRETAGNE REGION SELECTION ===========")
 df_bretagne = region_selection(df, 'BRETAGNE')
 print(df_bretagne.head())
 print()
-print("===== IDF REGION SELECTION =====")
+print("========== IDF REGION SELECTION ==========")
 df_idf = region_selection(df, 'ILE DE FRANCE')
+print(df_idf.head())
+print()
+print("========== RACA REGION SELECTION ==========")
+df_idf = region_selection(df, 'RHONE ALPES COTES D_AZUR')
 print(df_idf.head())
 print()
